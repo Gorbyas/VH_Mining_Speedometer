4dba438ff1799491fcfc9b47c7d710c6f6f7d0a3
jakstano <stanojakub03@gmail.com>
2024-06-16 23:28:26 +0200

    added a comment

--- src/main/java/net/gorbyas/miningspeedometer/MiningSpeedometer.java
+++ src/main/java/net/gorbyas/miningspeedometer/MiningSpeedometer.java
@@ -30,7 +30,7 @@ public class MiningSpeedometer {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
-
+// comment
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
