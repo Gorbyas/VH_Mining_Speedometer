1d7d99681579911c2d67af98fc07aa472f1a9b7b
jakstano <stanojakub03@gmail.com>
2024-06-18 17:05:11 +0200

    Updated mod description

--- src/main/java/net/gorbyas/miningspeedometer/commands/GetMiningspeedCommand.java
+++ src/main/java/net/gorbyas/miningspeedometer/commands/GetMiningspeedCommand.java
@@ -5,29 +5,44 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.item.ItemArgument;
+import net.minecraft.commands.arguments.item.ItemInput;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.util.*;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class GetMiningspeedCommand {
     public GetMiningspeedCommand(CommandDispatcher<CommandSourceStack> dispatcher){
-        dispatcher.register(Commands.literal("speedometer").then(Commands.literal("set").executes((command) -> {
+        dispatcher.register(Commands.literal("speedometer").then(Commands.literal("for").then(Commands.argument("item", ItemArgument.item())).executes((command) -> {
             return 0;
         })));
     }
 
     private boolean isVaultTool(ItemStack itemStack){
-        if(itemStack.isEmpty()){
-            return false;
-        } else if (itemStack.getTag() == null) {
+        if(itemStack.getTag() == null || itemStack.isEmpty()){
             return false;
         } else return itemStack.getTag().contains("vault_tool");
     }
 
-    private int getMiningspeed(CommandSourceStack source) throws CommandSyntaxException{
+    private int getMiningspeed(CommandSourceStack source, ItemArgument itemArgument) throws CommandSyntaxException{
         Inventory playerInventory = source.getPlayerOrException().getInventory();
-        ItemStack item = playerInventory.getSelected();
+        ItemStack item = playerInventory.player.getMainHandItem();
+        float toolSpeed;
+        float msNeeded;
         if (isVaultTool(item)){
+            toolSpeed = 0;
+            if (item.isEnchanted()){
+                if(item.getEnchantmentTags().contains("Efficiency")){
+                    toolSpeed *= 1.2f;
+                }
+            }
             return 1;
-        } else return 0;
+        }
+        source.sendSuccess(new TextComponent("You’ll need x more"), true);
+        return 0;
+
     }
 }
